9aeeb9330b657ec805a863b51812eb64829a5880
donako <donako@vishnus-air.fios-router.home>
2020-03-30 00:44:18 -0400

    assingment - 2

--- Random_Programs/src/Program1.java
+++ Random_Programs/src/Program1.java
@@ -1,9 +1,28 @@
 public class Program1 {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		Program1 obj = new Program1();
+		int arr[] = {1,1,2,2,2,4,4,4,4};
 		
-		
-		System.out.println("Hello World");
+		System.out.println(obj.findLucky(arr));
 	}
+	
 
+public int findLucky(int[] arr) {
+	        
+	int count;
+	int lucky = -1;
+	    for(int i=0; i<arr.length; i++){
+	    count=0;
+	    	for(int j=0; j<arr.length; j++){
+	    		if(arr[i] == arr[j]){
+	                count++;
+	            }
+	        }
+	    	if(count > lucky){
+	    		lucky = count;
+	    	}
+	    }
+	return lucky;
+	}
 }
